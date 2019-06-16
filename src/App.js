addf806276914a880aed412bdf3f10241ec3acfe
scott <scottarthurgall@gmail.com>
2019-06-15 21:42:12 -0400

    added styling and playlist component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  const name = 'boi';
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Hello, {name}</h1>
-      </header>
-      <p className="App-intro">
+let textColor = 'white';
+let defaultStyle = {
+  color: textColor
+}
 
-      </p>
-    </div>
-  );
+class Aggregate extends Component {
+  render() {
+    return (
+      <div style={{...defaultStyle, width: "40%", display: "inline-block"}}>
+        <h2>Number Test</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return (
+      <div style={defaultStyle}>
+        <img />
+        <input type="text" />
+        
+      </div>
+    )
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return (
+      <div style={{...defaultStyle, display: "inline-block", width: "25%"}}>
+        <img />
+        <h3>Playlist Name</h3>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>Song 3</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    const name = 'boi';
+    let pink = 'pink';
+    let headerStyle = {color: pink, 'fong-size': '50px'};
+    return (
+      <div className="App">
+        <h1>TITLE</h1>
+        <Aggregate />
+        <Aggregate />
+        <Filter />
+        <Playlist />
+        <Playlist />
+        <Playlist />
+        <Playlist />
+      </div>
+    );
+  }
 }
 
 export default App;
